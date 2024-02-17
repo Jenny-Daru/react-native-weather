bec7dfbde21ef3a5b7e1046636ac52e2f534d103
jenny <dooly3392@gmail.com>
2024-02-17 23:44:53 +0900

    View Layout의 flex style

--- App.js
+++ App.js
@@ -3,18 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>First app</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "row"}}>
+      <View style={{ flex: 1, backgroundColor: "tomato"}}></View>
+      <View style={{ flex: 3, backgroundColor: "teal"}}></View>
+      <View style={{ flex: 1, backgroundColor: "orange"}}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
